877fd71bc5d0d95fc3fca8ca816a8aec61c66088
Julie Hunt <jhunt@jhunt-m1.local>
2018-05-15 17:19:23 -0500

    debugging 503 error on heroku

--- config/main.js
+++ config/main.js
@@ -2,5 +2,5 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'fluffy hoppy soft buns',
   // Database connection information
-  'database': process.env.MONGODB_URI || 'mongodb://theladyjoules:ml13babaR@ds231199.mlab.com:31199/mindful_moderation',
+  'database': 'mongodb://theladyjoules:ml13babaR@ds231199.mlab.com:31199/mindful_moderation',
 }
